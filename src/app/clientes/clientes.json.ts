e8101939cd65ad0073d1b8dfc0c298542db2e221
calyluna2019 <calyluna87@gmail.com>
2020-05-31 02:24:26 -0300

    Servicios

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,18 @@
+import { Cliente } from './cliente';
+
+
+export const CLIENTES: Cliente[] = [
+    {id:1, nombre:'Jose', apellido:'Ramirez', email:'joramirex@hotmail.com', createAs:'2020-12-11'},
+    {id:2, nombre:'Maria', apellido:'Perez', email:'joramiresdx@hotmail.com', createAs:'2020-12-10'},
+    {id:3, nombre:'Pedro', apellido:'Luna', email:'joreeeirex@hotmail.com', createAs:'2020-12-9'},
+    {id:4, nombre:'Juan', apellido:'Nieva', email:'jora212ex@hotmail.com', createAs:'2020-12-8'},
+    {id:5, nombre:'Javier', apellido:'Guitierrez', email:'joram32irex@hotmail.com', createAs:'2020-12-7'},
+    {id:6, nombre:'Aliejandra', apellido:'Ramirez', email:'jodfex@hotmail.com', createAs:'2020-12-6'},
+    {id:7, nombre:'angela', apellido:'Gonzales', email:'jora2d23ex@hotmail.com', createAs:'2020-12-5'},
+    {id:8, nombre:'Karina', apellido:'Abasto', email:'158irex@hotmail.com', createAs:'2020-12-4'},
+    {id:9, nombre:'Enzo', apellido:'Pereyra', email:'joraaulax@hotmail.com', createAs:'2020-12-3'},
+    {id:10, nombre:'Alberto', apellido:'Moris', email:'jjavyrex@hotmail.com', createAs:'2020-12-2'},
+    {id:11, nombre:'Ariel', apellido:'Rivera', email:'jaleirex@hotmail.com', createAs:'2020-12-1'},
+    {id:12, nombre:'Agustin', apellido:'Lopez', email:'pelotarex@hotmail.com', createAs:'2020-12-24'},
+    {id:13, nombre:'Manuel', apellido:'Camvubi', email:'joramsssssdsirex@hotmail.com', createAs:'2020-12-23'}
+  ];
